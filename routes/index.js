f92b7df5eb83a342584f733a02de4b4ea7ddd0e7
RobertsonMark <robertsonmark@outlook.com>
2019-01-26 18:10:45 -0500

    init

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/reserve', function(req, res, next) {
+  console.log("not gay");
+  //logic connecting to db
+  res.send('/');
+});
+
+module.exports = router;
